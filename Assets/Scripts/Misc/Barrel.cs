974cc2abcc1b2e0bc39e4f27ca1aa3923256ea92
anderer455 <andrejurbanek455@gmail.com>
2023-05-07 13:17:53 +0200

    Created interactible neutral miscs

---
+++ Assets/Scripts/Misc/Barrel.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrel : MonoBehaviour
+{
+    [SerializeField] private GameObject explosionPrefab;
+    private bool isDestroyed = false;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.TryGetComponent<Projectile>(out Projectile projectile)) {
+            DestroyBarrel();
+        }
+    }
+
+    private void DestroyBarrel() {
+        if (!isDestroyed) {
+            isDestroyed = true;
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
