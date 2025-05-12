fb6511cc5407428b1a99e78cfd8bf6da5ddf4191
anderer455 <andrejurbanek455@gmail.com>
2025-05-12 18:08:45 +0200

    upload WormsDP build

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Screen.SetResolution(1280, 720, false); // width, height, fullscreen
     }
 
     // Update is called once per frame
